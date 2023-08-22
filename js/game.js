ba79a179bd0833568d37cd73b2ce9d7084e84155
devpedromelo <116653056+devpedromelo@users.noreply.github.com>
2023-08-22 15:11:21 -0300

    criando o arquivo de controle

--- js/game.js
+++ js/game.js
@@ -12,11 +12,13 @@ let prevTime = 0;
 function animate(){
     window.requestAnimationFrame(animate);
 
+    handleControls()
+
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvasWidth, canvasHeight);
 
     player.uptade();
-    player2.uptade();
+    // player2.uptade();
 
     let delta = (performance.now() - prevTime) / 1000;
     let fps = 1/delta;
--- js/sprites.js
+++ js/sprites.js
@@ -1,4 +1,4 @@
-const gravity = 0.2;
+const gravity = 0.6;
 
 class Sprite {
     constructor({ position, velocity, dimensions }){
@@ -14,11 +14,11 @@ class Sprite {
     }
 
     uptade(){
-
         this.velocity.y += gravity;
 
-        if(this.position.y + this.height >= canvas.height){
-            this.velocity.y = canvas.height - (this.position.y + this.height)
+        if(this.position.y + this.height > canvas.height){
+            this.position.y = canvas.height - this.height
+            this.velocity.y = 0
         }else{
             this.velocity.y += gravity
         }
@@ -30,7 +30,26 @@ class Sprite {
     }
 }
 
-const player = new Sprite({
+class Fighter extends Sprite{
+    constructor({
+        position,
+        velocity,
+        dimensions
+    }) {
+        super({
+            position,
+            velocity,
+            dimensions
+        })
+        this.velocity = velocity
+        this.width = dimensions.width
+        this.height = dimensions.height
+
+        this.lastKeyPressed
+    }
+}
+
+const player = new Fighter({
     position: {
         x: 100,
         y: 0
@@ -45,9 +64,9 @@ const player = new Sprite({
     }
 })
 
-const player2 = new Sprite({
+const player2 = new Fighter({
     position: {
-        x: 500,
+        x:300,
         y: 20
     },
     velocity:{
